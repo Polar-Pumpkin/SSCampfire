39528003b21a548c521aea9aa5d0dc6b315d2071
EntityParrot_ <entityparrot@qq.com>
2021-01-20 01:25:17 +0800

    [+] 新增命令
    本次 Commit 包含以下内容:
    - [+] 新增了 Reload 等基础命令(/sscampfire).

---
+++ src/main/java/org/serverct/parrot/sscampfire/command/CommandHandler.java
@@ -0,0 +1,21 @@
+package org.serverct.parrot.sscampfire.command;
+
+import org.serverct.parrot.parrotx.api.ParrotXAPI;
+import org.serverct.parrot.parrotx.command.subcommands.DebugCommand;
+import org.serverct.parrot.parrotx.command.subcommands.HelpCommand;
+import org.serverct.parrot.parrotx.command.subcommands.ReloadCommand;
+import org.serverct.parrot.parrotx.command.subcommands.VersionCommand;
+import org.serverct.parrot.parrotx.data.autoload.Autoload;
+import org.serverct.parrot.sscampfire.SSCampfire;
+
+@Autoload
+public class CommandHandler extends org.serverct.parrot.parrotx.command.CommandHandler {
+    public CommandHandler() {
+        super(ParrotXAPI.getPlugin(SSCampfire.class), "sscampfire");
+
+        register(new ReloadCommand(plugin, "SSCampfire.admin"));
+        register(new DebugCommand(plugin, "SSCampfire.admin"));
+        register(new HelpCommand(plugin));
+        register(new VersionCommand(plugin));
+    }
+}
